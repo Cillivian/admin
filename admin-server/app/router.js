5ced66e736976f7c73a463bcdaecbda3c47dad91
Cillivian <h2ojeremy@hotmail.com>
2019-07-04 00:17:12 +0800

    fix

--- admin-server/app/router.js
+++ admin-server/app/router.js
@@ -4,8 +4,8 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-    const { router, controller } = app;
-    router.get('/', controller.home.index);
-    router.resources('user', '/user', controller.users);
-    // router.get('/user', controller.user.index);
+  const { router, controller } = app;
+  router.get('/', controller.home.index);
+  router.resources('user', '/user', controller.users);
+  // router.get('/user', controller.user.index);
 };
